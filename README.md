53e5f7092a8d2d8cb3f36ed9f473aeacf102897f
Joe Wills <JGW3Code@gmail.com>
2020-05-22 10:00:48 -0500

    finally working

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Password_Generator
 VCB_HW_03
+
+
+The assignment was to properly code a password generator that would announce to the end user certain prompts for the conditions that must be met to make a proper password. These conditions include amount of characters and which characters would be used. Each time the end user made a selection the program would advance or prompt the end user to make another selection until a proper password was created. 
+
--- script.js
+++ script.js
@@ -4,70 +4,79 @@ var generateBtn = document.querySelector("#generate");
 //Arrays for the Password
 var typesOfCharacters = {
   lowercase: "abcdefghijklomnopqrstuvwxyz",
-  specialCharacters: "!@#$%^&*()-_+=<>?\[]{}|~`",
-}
-console.log(totalCharacters);
+  specialCharacters: "!@#$%^&*()-_+=<>?[]{}|~`",
+};
 
 generateBtn.addEventListener("click", writePassword);
 
 function generatePassword() {
-  var fromPrompt = prompt("For your password, enter a number of characters you want to use, between 8 and 128");
-  var totalCharacters = parseInt(fromPrompt);
+  var fromPrompt = prompt(
+    "For your password, enter a number of characters you want to use, between 8 and 128"
+  );
+  var length = parseInt(fromPrompt);
 
   if (length < 8 || length > 128) {
-  alert ("Password must be more than 8 characters, but less than 128.");
-  return "Please select again!";
+    alert("Password must be more than 8 characters, but less than 128.");
+    return "Please select again!";
   }
 
-  if (!lenght) {
-  alert ("Password must be more than 8 characters, but less than 128.");
-  return "Please select again!";
-}
-
-var useLowerCase = confirm("Would you like to use lowercase letters in your password?");
-var useUpperCase = confirm("Would you like to use uppercase letters in your password?");
-var useSymbols = confirm("Would you like to use symbols in your password?");
-var useNumbers = confirm("Would you like to use some numbers in your password?");
-
-console.log("L: ", useLowerCase);
-console.log("U: ", useUpperCase);
-console.log("S: ", useSymbols);
-console.log("N: ", useNumbers);
-
-if (!useLowerCase && !useUpperCase && !useSymbols && !useNumbers) {
-  alert ("You must choose at least one uppercase, lowercase, symbol or number for your Password");
-  return "Please select again!";
-}
-
-var possibleCharacters = [];
-
-if (useLowerCase) {
-  possibleCharacters = possibleCharacters.concat(typesOfCharacters.lowercase.split(""));
-}
-if (useUpperCase) {
-  possibleCharacters = possibleCharacters.concat(typesOfCharacters.lowercase.toUpperCase().split(""));
-}
-if (useSymbols) {
-  possibleCharacters = possibleCharacters.concat(typesOfCharacters.specialCharacters.split(""));
-}
-if (useNumbers) {
-  for (var i = 0; i < 10; i++) {
-    possibleCharacters.push(i);
+  if (!length) {
+    alert("Password must be more than 8 characters, but less than 128.");
+    return "Please select again!";
   }
-}
 
-console.log(possibleCharacters)
+  var useLowerCase = confirm(
+    "Would you like to use lowercase letters in your password?"
+  );
+  var useUpperCase = confirm(
+    "Would you like to use uppercase letters in your password?"
+  );
+  var useSymbols = confirm("Would you like to use symbols in your password?");
+  var useNumbers = confirm(
+    "Would you like to use some numbers in your password?"
+  );
+  console.log ("conditions")
+  if (!useLowerCase && !useUpperCase && !useSymbols && !useNumbers) {
+    alert(
+      "You must choose at least one uppercase, lowercase, symbol or number for your Password"
+    );
+    return "Please select again!";
+  }
+  console.log ("possibleCharacters")
+  var possibleCharacters = [];
 
-var password = "";
+  if (useLowerCase) {
+    possibleCharacters = possibleCharacters.concat(
+      typesOfCharacters.lowercase.split("")
+    );
+  }
+  if (useUpperCase) {
+    possibleCharacters = possibleCharacters.concat(
+      typesOfCharacters.lowercase.toUpperCase().split("")
+    );
+  }
+  if (useSymbols) {
+    possibleCharacters = possibleCharacters.concat(
+      typesOfCharacters.specialCharacters.split("")
+    );
+  }
+  if (useNumbers) {
+    for (var i = 0; i < 10; i++) {
+      possibleCharacters.push(i);
+    }
+  }
+  console.log ("conditionsMet")
+  var password = "";
 
-for (var i = 0; 1 < totalCharacters; i++) {
-  var position = Math.floor(Math.random() * possibleCharacters.length);
-  password = password + possibleCharacters[position];
-}
-
+  for (var i = 0; i < length; i++) {
+    var position = Math.floor(Math.random() * possibleCharacters.length);
+    password = password + possibleCharacters[position];
+  }
+  console.log("PASSWORD: ", password);
   return password;
-
+}
 function writePassword() {
-    var password = generatePassword();
-    var passwordText = document.querySelector(password),
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+  passwordText.value = password;
 }
